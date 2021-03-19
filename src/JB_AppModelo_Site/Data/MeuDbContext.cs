8f160647eb0b886a9108b38ca5f57d57e1fd6f9a
João Batista <joaobatista@protechsolutions.com.br>
2021-03-19 09:58:12 -0300

    Configuração do DbContext

---
+++ src/JB_AppModelo_Site/Data/MeuDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JB_AppModelo_Site.Data
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+    }
+}
--- src/JB_AppModelo_Site/Startup.cs
+++ src/JB_AppModelo_Site/Startup.cs
@@ -1,14 +1,11 @@
 using JB_AppModelo_Site.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace JB_AppModelo_Site
 {
@@ -16,6 +13,14 @@ namespace JB_AppModelo_Site
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<RazorViewEngineOptions>(options =>
@@ -25,6 +30,8 @@ namespace JB_AppModelo_Site
                 options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
                 options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
             });
+
+            services.AddDbContext<MeuDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MeuDbContext")));
 
             services.AddMvc().AddMvcOptions(x => x.EnableEndpointRouting = false);
 
